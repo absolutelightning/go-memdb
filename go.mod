583c9ce4689b0dd9ad435c3c600be915c1fcc899
Ashesh Vidyut <ashesh.vidyut@hashicorp.com>
2024-06-26 08:12:16 +0530

    Using RefCount and Lazy MutateChannel in IRadix

--- go.mod
+++ go.mod
@@ -1,8 +1,12 @@
 module github.com/hashicorp/go-memdb
 
-go 1.13
+go 1.21.5
+
+toolchain go1.22.4
 
 require (
-	github.com/hashicorp/go-immutable-radix v1.3.1
-	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/absolutelightning/go-immutable-radix v1.0.2
+	github.com/hashicorp/go-uuid v1.0.3
 )
+
+require github.com/hashicorp/golang-lru/v2 v2.0.0 // indirect
--- memdb.go
+++ memdb.go
@@ -6,11 +6,10 @@
 package memdb
 
 import (
+	iradix "github.com/absolutelightning/go-immutable-radix"
 	"sync"
 	"sync/atomic"
 	"unsafe"
-
-	"github.com/hashicorp/go-immutable-radix"
 )
 
 // MemDB is an in-memory database providing Atomicity, Consistency, and
@@ -28,7 +27,7 @@ import (
 // snapshots of the DB being read from other goroutines.
 type MemDB struct {
 	schema  *DBSchema
-	root    unsafe.Pointer // *iradix.Tree underneath
+	root    unsafe.Pointer // *iradix.Tree[any] underneath
 	primary bool
 
 	// There can only be a single writer at once
@@ -45,7 +44,7 @@ func NewMemDB(schema *DBSchema) (*MemDB, error) {
 	// Create the MemDB
 	db := &MemDB{
 		schema:  schema,
-		root:    unsafe.Pointer(iradix.New()),
+		root:    unsafe.Pointer(iradix.New[any]()),
 		primary: true,
 	}
 	if err := db.initialize(); err != nil {
@@ -64,8 +63,11 @@ func (db *MemDB) DBSchema() *DBSchema {
 }
 
 // getRoot is used to do an atomic load of the root pointer
-func (db *MemDB) getRoot() *iradix.Tree {
-	root := (*iradix.Tree)(atomic.LoadPointer(&db.root))
+func (db *MemDB) getRoot(clone bool) *iradix.Tree[any] {
+	root := (*iradix.Tree[any])(atomic.LoadPointer(&db.root))
+	if clone {
+		root = root.Clone()
+	}
 	return root
 }
 
@@ -78,7 +80,7 @@ func (db *MemDB) Txn(write bool) *Txn {
 	txn := &Txn{
 		db:      db,
 		write:   write,
-		rootTxn: db.getRoot().Txn(),
+		rootTxn: db.getRoot(false).Txn(write),
 	}
 	return txn
 }
@@ -92,7 +94,7 @@ func (db *MemDB) Txn(write bool) *Txn {
 func (db *MemDB) Snapshot() *MemDB {
 	clone := &MemDB{
 		schema:  db.schema,
-		root:    unsafe.Pointer(db.getRoot()),
+		root:    unsafe.Pointer(db.getRoot(true)),
 		primary: false,
 	}
 	return clone
@@ -101,10 +103,10 @@ func (db *MemDB) Snapshot() *MemDB {
 // initialize is used to setup the DB for use after creation. This should
 // be called only once after allocating a MemDB.
 func (db *MemDB) initialize() error {
-	root := db.getRoot()
+	root := db.getRoot(false)
 	for tName, tableSchema := range db.schema.Tables {
 		for iName := range tableSchema.Indexes {
-			index := iradix.New()
+			index := iradix.New[any]()
 			path := indexPath(tName, iName)
 			root, _, _ = root.Insert(path, index)
 		}
