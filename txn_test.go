d6a38f29072837fe941e99e4d6f507bbcb96da38
Ashesh Vidyut <ashesh.vidyut@hashicorp.com>
2024-07-15 21:25:11 +0530

    fix bench

--- txn_test.go
+++ txn_test.go
@@ -2237,7 +2237,7 @@ func testDBB(b *testing.B) *MemDB {
 	return db
 }
 
-func BenchmarkTxnInsert(b *testing.B) {
+func BenchmarkTxnIterator(b *testing.B) {
 	db := testDBB(b)
 	txn := db.Txn(true)
 
